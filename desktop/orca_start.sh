f633115c1026988d672d8a76e783cfa5b7e0f41b
smonff <seb@feugere.net>
2023-03-24 18:51:50 +0100

    Minor fixes

--- desktop/orca_start.sh
+++ desktop/orca_start.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+# XXX Should be generic
 ORCA_HOME=/home/smonff/Orca-linux-arm64
 ORCA_BIN=Orca
 
 cd ${ORCA_HOME}
 
 # Would crash on ARM64 ships with the sandbox enabled
-./${ORCA_BIN} --no-sandbox 
+./${ORCA_BIN} --no-sandbox &
 
